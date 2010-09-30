dbc4c7b54df52c184e98457d00f9f2d30c67658c
Hidden <zzdhidden@gmail.com>
2010-09-30 11:31:25 +0800

    UP UI

--- static/i18n/webim-en.js
+++ static/i18n/webim-en.js
@@ -52,9 +52,8 @@ webim.ui.i18n.store('en',{
 	"chat with":"Chat with {{name}}",
 	"chat with me":"Chat with me",
 	"new message":"New Message",
-	"connect error":"Connect error",
-	"disconnect":"Disconnect",
-	"online error":"Failed to online",
+	"Connect Error":"Connect error",
+	"Disconnect":"Disconnect",
 	"play_sound": "Play sound for new messages",
 	"buddy_sticky": "Keep friends window open",
 	"msg_auto_pop": "Auto pop-up window for new message",
@@ -81,10 +80,22 @@ webim.ui.i18n.store('en',{
 	"unavailable": "Unavailable",
 	"online support": "online support",
 	"location": "location",
-	"from": "from"
+	"from": "from",
+	"download history": "Download histories",
+	"font color": "Font color",
+	"Not Found": "Not Found",
+	"Forbidden": "Forbidden",
+	"Not Authorized" : "Not Authorized",
+	"IM Server Not Found": "IM Server Not Found",
+	"IM Server Not Authorized": "IM Server Not Authorized",
+	"username": "Username",
+	"password": "Password",
+	"question": "Question",        
+	"answer": "Answer",
+	"login": "Login"
 });
 
-webim.ui.i18n.store('zh-CN',{
+webim.ui.i18n.store('en',{
 	"doing": "Record",
 	"album": "Photo",
 	"blog": "Note",
